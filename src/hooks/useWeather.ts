0dc528b889010c76d3bd9b79c8a78ef35a530bb1
TKSOHLT <kimisami31@gmail.com>
2025-07-10 14:28:20 -0600

    🐛Fixed: cambio de url de http a https

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -67,7 +67,7 @@ export default function useWeather() {
     setWeather(initialState);
     setNotFound(false);
     try {
-      const geoURl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+      const geoURl = `https://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
       const { data } = await axios(geoURl); //Por defecto al hacer axios() es como hacer axios.get()...
 
       if(!data[0]){
